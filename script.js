dfa9835df169b864c569407953c9e7a1be72e9ac
fullstackraven <fullstackraven@gmail.com>
2022-10-15 15:33:33 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+            const output = document.querySelector('.output');
+            output.innerHTML = '';
+            
+                for (let i = 1; i <= answer; i++) {
+                    const para = document.createElement('p');
+                    if (i % 3 === 0 && i % 5 === 0) {
+                        para.textContent = `FizzBuzz`;
+                        console.log("FizzBuzz");
+                    } else if (i % 3 === 0) {
+                        para.textContent = 'Fizz';
+                        console.log("Fizz");
+                    } else if (i % 5 === 0) {
+                        para.textContent = 'Buzz';
+                        console.log("Buzz");
+                    } else {
+                        para.textContent = i;
+                        console.log(i);
+                    }
+
+                    output.appendChild(para);
+
+                }
+
+                const clearBtn = document.querySelector('.clear');
+
+                clearBtn.addEventListener('click', () => para.textContent = '');
